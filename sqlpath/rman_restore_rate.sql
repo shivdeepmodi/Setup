ea5f1795b658cbc7c38c47069c140cb7d275cf4d
shivdeepmodi <39369164+shivdeepmodi@users.noreply.github.com>
2021-07-01 15:05:10 +0530

    Create rman_restore_rate.sql

---
+++ sqlpath/rman_restore_rate.sql
@@ -0,0 +1,29 @@
+column Pct_Complete format 99.99
+column client_info format a25
+column sid format 999
+column MB_PER_S format 999.99
+select s.client_info,
+l.sid,
+l.serial#,
+l.sofar,
+l.totalwork,
+round (l.sofar / l.totalwork*100,2) "Pct_Complete",
+aio.MB_PER_S,
+aio.LONG_WAIT_PCT
+from v$session_longops l,
+v$session s,
+(select sid,
+serial,
+100* sum (long_waits) / sum (io_count) as "LONG_WAIT_PCT",
+sum (effective_bytes_per_second)/1024/1024 as "MB_PER_S"
+from v$backup_async_io
+group by sid, serial) aio
+where aio.sid = s.sid
+and aio.serial = s.serial#
+and l.opname like 'RMAN%'
+and l.opname not like '%aggregate%'
+and l.totalwork != 0
+and l.sofar <> l.totalwork
+and s.sid = l.sid
+and s.serial# = l.serial#
+order by 1;
